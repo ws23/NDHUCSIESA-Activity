0104cbbaa08f2200f1a395a32ea72a06d9c9f8e1
ws23 <linyutsu23@gmail.com>
2013-09-25 01:48:40 +0800

    Update money.php

--- money.php
+++ money.php
@@ -11,7 +11,7 @@ if(isset($_POST['activity'])){
                 <label class="create_label">學號</label>
                 <input type="text" class="create_input" name="stuID">
                 <input type="hidden" name="activity" value="' . $_POST['activity'] . '">
-                <input type="submit" value="報到">
+                <input type="submit" value="繳費">
         <form>
         ';
 }
@@ -31,5 +31,34 @@ if(isset($_POST['stuID'])){
 		echo "<br>" . $row2[2] . "繳費成功。</br>";
 	}
 }
-require("footer.php"); ?>
 
+
+        echo '<table>';
+        echo '<tr align="center" style="background-color: yellow;"><td width="200px">繳費</td><td width="200px">學號</td><td width="200px">姓名</td><td width="200px">繳費與否</td><td width="200px">報到與否</td><td width="200px">中獎與否</td>';
+        $result = mysql_query("SELECT `TName` FROM `main` WHERE `AID` = {$_POST['activity']};");
+        $TName = mysql_fetch_row($result);
+
+        $result = mysql_query("SELECT * FROM `{$TName[0]}` ORDER BY `stuID`;");
+        while($row = mysql_fetch_row($result)){
+		if($row[4])
+			continue;
+		echo '<tr align="center"><td>';
+		echo'
+		<form method="post">
+	        	<input type="hidden" name="activity" value="' .  $_POST['activity'] . '">
+			<input type="hidden" name="stuID" value="' . $row[1] . '">
+	                <input type="submit" value="繳費" style="font-size: 12pt; border: 400;" >			
+		</form>
+		';
+                echo "<td>{$row[1]}</td><td>{$row[2]}</td><td>";
+                echo $row[4]?"已繳費":"未繳費";
+                echo "</td><td>";
+                echo $row[5]?"已報到":"未報到";
+                echo "</td><td>";
+                echo $row[6]?"有中獎":"沒中獎";
+                echo "</td></tr>";
+        }
+        echo "</table>";
+
+
+require("footer.php"); ?>
