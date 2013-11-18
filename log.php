e2d3b7e060b6670401637a6a13d30e47f03bb2e2
ws23 <root@ws23-Aspire-V5-471G.(none)>
2013-11-18 22:26:56 +0800

    add log

---
+++ log.php
@@ -0,0 +1,31 @@
+<?php
+
+       function GetIP(){
+
+                if(!empty($_SERVER["REMOTE_ADDR"])){
+                        $ip = $_SERVER["REMOTE_ADDR"];
+                }
+
+                if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                        $ips = explode (",", $_SERVER['HTTP_X_FORWARDED_FOR']);  
+                        if ($ip) { array_unshift($ips, $ip); $ip=FALSE; }
+                        for ($i=0; $i < count($ips); $i++) {
+                                if (!eregi ("^(10|172.16|192.168).", $ips[$i])) {
+                                        $ip=$ips[$i];
+                                        break;
+                                }
+                        }
+                }
+
+                return $ip;
+        }
+
+
+	function LogBook($text){
+		$now = date("Y-m-d H:i:s", time());
+		$IP = GetIP();
+		mysql_query("INSERT INTO `log` (`time`, `content`, `IP`) VALUES('{$now}', '{$text}', '{$IP}');");
+	}
+	
+
+?>
