223d7b26e506aa1e2fc0ada248311a4f6b305ad1
ws23 <root@ws23-Aspire-V5-471G.(none)>
2013-11-23 14:40:38 +0800

    update the table of payment

--- admin/payment.php
+++ admin/payment.php
@@ -1,8 +1,6 @@
 <?php 
 require("header.php"); 
 
-echo '<table>';
-echo '<tr align="center" style="background-color: yellow;"><td width="200px">學號</td><td width="200px">姓名</td><td width="200px">應繳金額</td><td width="200px">繳費與否</td><td width="200px">報到與否</td><td width="200px">中獎與否</td>';
 $result = mysql_query("SELECT `TName`, `charge`, `chargeMember`, `back`, `backMember` FROM `main` WHERE `AID` = {$_POST['activity']};");
 $TName = mysql_fetch_array($result);
 
@@ -32,8 +30,8 @@ while($row = mysql_fetch_array($result)){
 }
 echo "共 {$count} 人報名、{$countPay} 人已繳費、{$countCheck} 人已報到。<br /><br /><br />\n";
 ?>
-<table align="center" valign="middle">
-<tr><th></th><th>收入</th><th>退費</th><th>小計</th></tr>
+<table align="center" valign="middle" style="font-size: 24pt; ">
+<tr><th width="200px"></th><th width="200px">收入</th><th width="200px">退費</th><th width="200px">小計</th></tr>
 <tr><td>會員</td><td><?php echo $incomeM; ?></td><td><?php echo $expenditureM; ?></td><td><?php echo $incomeM-$expenditureM; ?></td></tr>
 <tr><td>非會員</td><td><?php echo $income; ?></td><td><?php echo $expenditure; ?></td><td><?php echo $income-$expenditure; ?></td></tr>
 <tr><td colspan="4">----------------------------------</td></tr>
