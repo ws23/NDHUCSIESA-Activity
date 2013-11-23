abf07d7ca56f2bcea22e03d8ead44321880057aa
ws23 <root@ws23-Aspire-V5-471G.(none)>
2013-11-23 14:50:19 +0800

    fix the bug of payment table

--- admin/payment.php
+++ admin/payment.php
@@ -6,17 +6,21 @@ $TName = mysql_fetch_array($result);
 
 LogBook("admin/list.php", "觀看 {$_POST['activity']} 報名狀況。");
 $result = mysql_query("SELECT * FROM `{$TName['TName']}` ORDER BY `stuID`;");
-$count = $countPay = $countCheck = $income = $incomeM = $expenditure = $expenditureM = 0;
+$count = $countPay = $countCheck = $income = $incomeM = $expenditure = $expenditureM = $getM = $get = 0;
 while($row = mysql_fetch_array($result)){
 	$info = mysql_query("SELECT `isMember` FROM `stuinfo` WHERE `stuID` = '{$row['stuID']}';");
 	$isMember = mysql_fetch_array($info);
-	if($isMember){
+	if($isMember=='1'){
 		$incomeM += $TName['chargeMember'];
 		$expenditureM += $TName['backMember'];
+		if($row['money'])
+			$getM += $TName['chargeMember'];
 	}
 	else{
 		$income += $TName['charge'];
 		$expenditure += $TName['back'];
+		if($row['money'])
+			$get = $Tname['charge'];
 	}
 	$count ++;
         if($row['charge']==0){
@@ -31,11 +35,11 @@ while($row = mysql_fetch_array($result)){
 echo "共 {$count} 人報名、{$countPay} 人已繳費、{$countCheck} 人已報到。<br /><br /><br />\n";
 ?>
 <table align="center" valign="middle" style="font-size: 24pt; ">
-<tr><th width="200px"></th><th width="200px">收入</th><th width="200px">退費</th><th width="200px">小計</th></tr>
-<tr><td>會員</td><td><?php echo $incomeM; ?></td><td><?php echo $expenditureM; ?></td><td><?php echo $incomeM-$expenditureM; ?></td></tr>
-<tr><td>非會員</td><td><?php echo $income; ?></td><td><?php echo $expenditure; ?></td><td><?php echo $income-$expenditure; ?></td></tr>
-<tr><td colspan="4">----------------------------------</td></tr>
-<tr><td>總計</td><td><?php echo $incomeM+$income; ?></td><td><?php echo $expenditureM+$expenditure; ?></td><td><?php echo $incomeM+$income-$expendtitureM-$expenditure; ?></td></tr>
+<tr><th width="200px"></th><th width="150px">應收</th><th width="150px">實收</th><th width="150px">退費</th><th width="150px">小計</th></tr>
+<tr align="center"><td>會員</td><td><?php echo $incomeM; ?></td><td><?php echo $getM; ?></td><td><?php echo $expenditureM; ?></td><td><?php echo $incomeM-$expenditureM; ?></td></tr>
+<tr align="center"><td>非會員</td><td><?php echo $income; ?></td><td><?php echo $get; ?></td><td><?php echo $expenditure; ?></td><td><?php echo $income-$expenditure; ?></td></tr>
+<tr align="center"><td colspan="4">-------------------------------------------------------------------------------------------</td></tr>
+<tr align="center"><td>總計</td><td><?php echo $incomeM+$income; ?></td><td><?php echo $get+$getM; ?></td><td><?php echo $expenditureM+$expenditure; ?></td><td><?php echo $incomeM+$income-$expendtitureM-$expenditure; ?></td></tr>
 </table>
 <?php
 require("footer.php"); 
